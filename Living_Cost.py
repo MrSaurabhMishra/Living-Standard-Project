ec0c4e5f9eb2c7856fddf43e2b1bbb4e6e7582e4
saurabh mishra <saurabhmishra09714@gmail.com>
2025-08-13 13:43:23 +0530

    cleaning and sorting

--- Living_Cost.py
+++ Living_Cost.py
@@ -11,19 +11,16 @@ def fetch_living_cost_data(url,df=None):
         raise Exception("Failed to load page")
     data = response.json()
     df = pd.DataFrame(data[1])
-    df['date'] = pd.to_datetime(df['date'])
-    df.set_index('date', inplace=True)
+    df['samay'] = pd.to_datetime(df['date'])
+    df.set_index('samay', inplace=True)
     df = df[['value']]
     df.rename(columns={'value': 'Living Cost Index'}, inplace=True)
     df['Living Cost Index'] = pd.to_numeric(df['Living Cost Index'], errors='coerce')
     df.dropna(inplace=True)
     df.sort_index(inplace=True)
-    df.plot(figsize=(12, 6))
-    plt.title('Living Cost Index Over Time')
-    plt.xlabel('Year')
-    plt.ylabel('Living Cost Index')
-    plt.grid()
-    plt.show()
+    print(df.head(5))
+    
+   
     
     
     
