28f0749747596b3edfd6d29bbdbd9b88d88e5e66
saurabh mishra <saurabhmishra09714@gmail.com>
2025-08-13 13:17:11 +0530

    plotting graph

--- Living_Cost.py
+++ Living_Cost.py
@@ -5,14 +5,37 @@ import seaborn as sns
 import requests
 from bs4 import BeautifulSoup
 
-def fetch_living_cost_data(url):
+def fetch_living_cost_data(url,df=None):
     response = requests.get(url)
     if response.status_code != 200:
         raise Exception("Failed to load page")
     data = response.json()
     df = pd.DataFrame(data[1])
-    df.to_csv("living_cost_data.csv", index=False)
-    print("Data fetched and saved to living_cost_data.csv")
+    df['date'] = pd.to_datetime(df['date'])
+    df.set_index('date', inplace=True)
+    df = df[['value']]
+    df.rename(columns={'value': 'Living Cost Index'}, inplace=True)
+    df['Living Cost Index'] = pd.to_numeric(df['Living Cost Index'], errors='coerce')
+    df.dropna(inplace=True)
+    df.sort_index(inplace=True)
+    df.plot(figsize=(12, 6))
+    plt.title('Living Cost Index Over Time')
+    plt.xlabel('Year')
+    plt.ylabel('Living Cost Index')
+    plt.grid()
+    plt.show()
+    
+    
+    
+
+
+
+
+
+
 
 
 fetch_living_cost_data("https://api.worldbank.org/v2/country/IN/indicator/FP.CPI.TOTL?format=json")
+
+
+
